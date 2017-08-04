1b290b02239ce6a099579c037d86383c3f40e52c
Torstein Krause Johansen <torstein@escenic.com>
2017-08-04 10:54:29 +0800

    Fixed Python Linting warings + added area_teaser_options_placement_VF_7149

--- checks/__init__.py
+++ checks/__init__.py
@@ -1,6 +1,13 @@
+import ece_admin
+import visualise_single_item_complex_fields_VF_7124
+import harden_upload_binary_with_illegal_home_section_VF_7160
+import xhr_req_disables_www_auth_resp_VF_7159
+import area_teaser_options_placement_VF_7149
+
 __all__ = [
     "ece_admin",
     "visualise_single_item_complex_fields_VF_7124",
     "harden_upload_binary_with_illegal_home_section_VF_7160",
-    "xhr_req_disables_www_auth_resp_VF_7159"
+    "xhr_req_disables_www_auth_resp_VF_7159",
+    "area_teaser_options_placement_VF_7149"
 ]
